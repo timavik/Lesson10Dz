1f118f75a750f6ed4eba99cdf184fac13d77aff0
timavik <103438439+timavik@users.noreply.github.com>
2025-01-20 13:01:20 +0300

    Add files via upload

---
+++ Lesson10Dz/Lesson10Dz/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lesson10Dz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputUsers = Console.ReadLine();
+            try
+            {
+                if (inputUsers.Contains('!') || inputUsers.Contains('?') || inputUsers.Contains(':'))
+                {
+                    throw new ArgumentException("Недопустимый символ");
+                }
+                else
+                {
+                    Console.WriteLine(inputUsers);
+                }
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Обнаружен запрещённый символ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
